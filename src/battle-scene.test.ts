8e0f82e70f6d56b574458e1572db03b895635979
meepen <meepdarknessmeep@gmail.com>
2024-03-29 03:13:27 -0400

    add dependency cruiser

---
+++ src/battle-scene.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import BattleScene from "./battle-scene";
+
+describe("BattleScene", () => {
+  it("should exist", () => {
+    expect(BattleScene).toBeDefined();
+  });
+});
---
+++ src/ui/egg-hatch-scene.handler.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import EggHatchSceneHandler from './egg-hatch-scene-handler';
+
+describe('EggHatchSceneHandler', () => {
+  it('should exist', () => {
+    expect(EggHatchSceneHandler).toBeDefined();
+  });
+});
